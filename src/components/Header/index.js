fe0285e4e46dad65da004b85be02a2ac0dc0925a
andthedropout <andthedropout@gmail.com>
2026-02-18 20:26:54 -0700

    Add new projects/clients, update logos, fix lint errors

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.scss';
 import { Link } from 'react-router-dom';
-import { Logo, Email } from '../../images';
+import { Logo } from '../../images';
 
 export default function Header() {
 	return (
